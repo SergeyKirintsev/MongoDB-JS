37634babd4bdd7a69ea28d4bfb0caf98b04ba2a8
SergeyKirintsev <kserg80@gmail.com>
2021-02-28 21:49:00 +0300

    сделать добавление коллекций

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
-import pkg from 'mongodb';
+import pkg from "mongodb";
 const { MongoClient } = pkg;
 
-import students from './first.json';
-import overallStudents from './second.json';
-
-// process.exit(-1);
+import students from "./first.json";
+import overallStudents from "./second.json";
 
 const uri = "mongodb://localhost:27017";
 
-const client = new MongoClient(uri,  { useUnifiedTopology: true } );
-
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 async function run() {
   try {
     await client.connect();
 
-    const database = client.db('study');
-    const collection1= database.collection('students');
-    const collection2= database.collection('overallStudents');
+    const database = client.db("study");
+    const studentsCollection = database.collection("students");
+    const overallStudentsCollection = database.collection("overallStudents");
 
-    // Query for a movie that has the title 'Back to the Future'
-    // const query = { title: 'Back to the Future' };
-    // const movie = await collection.findOne(query);
-    // console.log(movie);
-
-    const result1 = await collection1.insertMany(students);
+    const result1 = await studentsCollection.insertMany(students);
     console.log(`${result1.insertedCount} documents were inserted`);
 
-    const result2 = await collection2.insertMany(overallStudents);
+    const result2 = await overallStudentsCollection.insertMany(overallStudents);
     console.log(`${result2.insertedCount} documents were inserted`);
-
-
-    
-
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
